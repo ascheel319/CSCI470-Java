3ffdd739e1066a66c11e426b584e0bb0e790d287
ascheel319 <cubsfan319@comcast.net>
2020-01-24 01:22:00 -0600

    first commit

---
+++ HW1/temporary.java
@@ -0,0 +1,9 @@
+public class temporary
+{
+
+public static void main(String [] arg)
+{
+	System.out.println("This is just a place holder so that I can get the github part for this class added");
+}
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSCI470-Java
+This is for my CSCI470 Java class that I am currently in at NIU during the spring of my senior year(2020). I am taking this class with Professor Freedman
