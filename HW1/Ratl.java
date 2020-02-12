8287690bb20bbd177e51bc8c1336badbce144781
ascheel319 <cubsfan319@comcast.net>
2020-02-11 21:50:55 -0600

    added tests

--- HW1/Ratl.java
+++ HW1/Ratl.java
@@ -1,2 +1,197 @@
-//this is temp before I put more in here
+public class Ratl
+{
+	private int valueNum;
+	private int valueDenom;
+	private int visibleNum;
+	private int visibleDenom;
 
+	// no-arg constructor
+	// creates a Ratl with value 0/1
+	public Ratl()
+	{
+		valueNum = 0;
+		valueDenom = 1;
+	}
+	// two-int constructor
+	// creates a Ratl with value num/denom
+	public Ratl(int num, int denom)
+	{
+		valueNum = num;
+		valueDenom = denom;
+	}
+	// Ratl constructor
+	// builds a new Ratl with the same values as its input
+	public Ratl(Ratl a)
+	{
+		valueNum = a.valueNum;
+		valueDenom = a.valueDenom;
+	}
+	// getters
+	public int getNum()
+	{
+		return valueNum;
+	}
+	public int getDenom()
+	{
+		return valueDenom;
+	}
+/*	public int getVisNum()
+	{
+		return visibleNum;
+	}
+	public int getVisDenom()
+	{
+		return visbileDenom;
+	}
+*/
+	// setters
+	public void setNum(int num)
+	{
+		valueNum = num;
+	}
+	public void setDenom(int denom)
+	{
+		valueDenom = denom;
+	}
+/*	public void setVisNum(int num)
+	{
+
+	}
+	public void setVisDenom(int denom)
+	{
+
+	}
+*/
+	// convert to string
+	@Override
+	public String toString()
+	{
+		return valueNum + "/" + valueDenom;
+	}
+
+	// reduce
+	// reduces a Ratl to the canonical form described above
+	// uses the gcd function
+	public void reduce()
+	{
+
+	}
+
+	// static gcd
+	public static int gcd(int a, int b)
+	{
+
+	}
+
+	// static negate
+	// given a/b, returns -a/b
+	public static Ratl negate(Ratl a)
+	{
+		//not sure if this will work or not
+		a.valueNum = a.valueNum * -1;
+	}
+
+	// static invert
+	// given a/b, returns b/a
+	public static Ratl invert(Ratl a)
+	{
+		//untested
+		int temp;
+		temp = a.valueNum;
+		a.valueNum = a.valueDenom;
+		a.valueDenom = temp;
+	}
+
+	// add
+	// adds b to the Ratl it is called with
+	public void add(Ratl b)
+	{
+		
+	}
+
+	// sub
+	// implemented with add and negate
+	public void sub(Ratl b)
+	{
+		
+	}
+
+	// mult
+	public void mult(Ratl b)
+	{
+		
+	}
+
+	// div
+	// implemented with mult and inverse
+	public void div(Ratl b)
+	{
+		
+	}
+
+	// static add
+	public static Ratl add(Ratl a, Ratl b)
+	{
+		
+	}
+
+	// static sub
+	public static Ratl sub(Ratl a, Ratl b)
+	{
+		
+	}
+
+	// static mult
+	public static Ratl mult(Ratl a, Ratl b)
+	{
+		
+	}
+
+	// static div
+	public static Ratl div(Ratl a, Ratl b)
+	{
+		
+	}
+
+	// member ident
+	public boolean ident(Ratl a)
+	{
+		
+	}
+
+	// member equiv
+	public boolean equiv(Ratl a)
+	{
+		
+	}
+
+	// static ident
+	public static boolean ident(Ratl a, Ratl b)
+	{
+		
+	}
+
+	// static equiv
+	public static boolean equiv(Ratl a, Ratl b)
+	{
+		if(a.valueNum == b.valueNum && a.valueDenom == b.valueDenom)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	// debugPrint
+	// builds a string from the fields of a Ratl object as follows:
+	// num value 3 denom value 5 num visible 6 denom visible 10
+	// you might find Integer.toString() useful
+	public String debugPrint()
+	{
+		
+	}
+
+
+}
---
+++ HW1/RatlTest.java
@@ -0,0 +1,138 @@
+
+public class RatlTest
+{
+  public static void main(String[] args) 
+    {
+      Ratl a = new Ratl(3, 5);
+      Ratl b = new Ratl(7, 11);
+      Ratl st = new Ratl(6, 10);
+      Ratl aa = new Ratl(3, 5);
+      Boolean x;
+      System.out.printf("%n");
+
+      System.out.printf("fields of a:  %s%n", a.debugPrint());
+      System.out.printf("fields of b:  %s%n", a.debugPrint());
+      System.out.printf("fields of st: %s%n%n", a.debugPrint());      
+      
+      x = (a == a);
+      System.out.printf("%-32s%b%n", "3/5 == the same 3/5", x);
+
+      x = (a == aa);
+      System.out.printf("%-32s%b%n", "3/5 == a different 3/5", x);
+      
+      x = a.equals(a);
+      System.out.printf("%-32s%b%n", "3/5 .equals the same 3/5", x);
+
+      x = a.equals(aa);
+      System.out.printf("%-32s%b%n", "3/5 .equals a different 3/5", x);
+      System.out.printf("%n");
+
+      // static test fns
+
+      System.out.printf("static:%n");
+      x = Ratl.ident(a, a);
+      System.out.printf("%-32s%b%n", "3/5 is ident to the same 3/5", x);
+
+      x = Ratl.ident(a, aa);
+      System.out.printf("%-32s%b%n", "3/5 is ident to a different 3/5", x);
+
+      x = Ratl.ident(a, st);
+      System.out.printf("%-32s%b%n%n", "3/5 is ident to 6/10", x);
+
+      x = Ratl.equiv(a, a);
+      System.out.printf("%-32s%b%n", "3/5 is equiv to the same 3/5", x);
+
+      x = Ratl.equiv(a, aa);
+      System.out.printf("%-32s%b%n", "3/5 is equiv to a different 3/5", x);
+
+      x = Ratl.equiv(a, st);
+      System.out.printf("%-32s%b%n%n", "3/5 is equiv to 6/10", x);
+
+      // member test fns
+
+      System.out.printf("member%n");
+      x = a.ident(a);
+      System.out.printf("%-32s%b%n", "3/5 is ident to the same 3/5", x);
+
+      x = a.ident(aa);
+      System.out.printf("%-32s%b%n", "3/5 is ident to a different 3/5", x);
+
+      x = a.ident(a, st);
+      System.out.printf("%-32s%b%n%n", "3/5 is ident to 6/10", x);
+
+      x = a.equiv(a);
+      System.out.printf("%-32s%b%n", "3/5 is equiv to the same 3/5", x);
+
+      x = a.equiv(aa);
+      System.out.printf("%-32s%b%n", "3/5 is equiv to a different 3/5", x);
+
+      x = a.equiv(st);
+      System.out.printf("%-32s%b%n%n", "3/5 is equiv to 6/10", x);
+
+      // add
+      
+      Ratl c;
+      c = Ratl.add(a, b);
+      System.out.printf("static: %s + %s = %s%n", a, b, c);
+
+      Ratl d = new Ratl(a);
+      d.add(b);
+      System.out.printf("member: %s + %s = %s%n%n", a, b, d);
+
+      // add with reduction
+      
+      a = new Ratl(3, 6);
+      b = new Ratl(14, 14);
+      c = Ratl.add(a, b);
+
+      System.out.printf("static: %s + %s = %s%n", a, b, c);
+      
+      d = new Ratl(a);
+      d.add(b);
+      System.out.printf("member: %s + %s = %s%n%n", a, b, d);
+
+      // subtract
+      
+      c = Ratl.sub(new Ratl(3, 5), new Ratl(7, 11));
+      System.out.printf("static: 3/5 - 7/11 = %s%n", c);
+
+      c = new Ratl(3, 5);
+      c.sub(new Ratl(7, 11));
+      System.out.printf("member: 3/5 - 7/11 = %s%n%n", c);
+
+      // multiply
+      
+      c = Ratl.mult(new Ratl(3, 6), new Ratl(7, 11));
+      System.out.printf("static: 3/6 * 7/11 = %s%n", c);
+
+      c = Ratl.mult(new Ratl(7, 11), new Ratl(3, 6));
+      System.out.printf("static: 7/11 * 3/6 = %s%n%n", c);
+
+      // divide
+      
+      c = Ratl.div(new Ratl(7, 11), new Ratl(7, 11));
+      System.out.printf("static: 7/11 / 7/11 = %s%n", c);
+
+      c = Ratl.div(new Ratl(14, 11), new Ratl(7, 11));
+      System.out.printf("static: 14/11 / 7/11 = %s%n", c);
+
+
+      System.out.printf("%n");
+      testGCD(6, 10);
+      testGCD(6, 6);
+      testGCD(10, 6);
+      testGCD(6, 12);
+      testGCD(6, 16);
+      testGCD(3, 5);
+      System.out.printf("%n");
+
+   } // end main
+
+  public static void testGCD(int a, int b)
+    {
+	System.out.printf("gcd of %d and %d is %d%n", a, b,
+			  Ratl.gcd(a, b));
+	return;
+    }
+
+} // end class RatlTest
